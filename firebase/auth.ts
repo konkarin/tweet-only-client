c6584ad739f70e6e709000566096c5542b58ad84
konkarin <konkarin.photo@gmail.com>
2021-08-11 22:12:06 +0900

    update: firebaseのログインを追加

---
+++ firebase/auth.ts
@@ -0,0 +1,48 @@
+import {
+  getAuth,
+  onAuthStateChanged,
+  signInWithPopup,
+  TwitterAuthProvider,
+} from "firebase/auth";
+
+const provider = new TwitterAuthProvider();
+
+const auth = getAuth();
+
+export const loginTwitter = async () => {
+  const result = await signInWithPopup(auth, provider).catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.error(errorCode, errorMessage);
+
+    return null;
+  });
+
+  if (result == null) return null;
+
+  const credential = TwitterAuthProvider.credentialFromResult(result);
+
+  if (credential === null) return null;
+
+  const token = credential.accessToken;
+  const secret = credential.secret;
+
+  if (token == null || secret == null) return null;
+
+  return { token, secret };
+};
+
+export const authStateChange = () => {
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      console.log("signed in", user);
+    } else {
+      console.log("signed out");
+    }
+  });
+};
+
+export const logoutTwitter = async () => {
+  await auth.signOut();
+  console.log("sign out");
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,36 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import { useState } from "react";
+import { statusesUpdate } from "../api/twitter";
+import { authStateChange, loginTwitter, logoutTwitter } from "../firebase/auth";
+import styles from "../styles/Home.module.css";
+
+authStateChange();
 
 export default function Home() {
+  const [token, setToken] = useState("");
+  const [isAuth, setIsAuth] = useState(false);
+
+  const login = async () => {
+    const credentials = await loginTwitter();
+    console.log(credentials);
+
+    if (credentials == null) return;
+
+    setToken(credentials.token);
+    setIsAuth(true);
+  };
+
+  const logout = async () => {
+    await logoutTwitter();
+
+    setToken("");
+    setIsAuth(false);
+  };
+
+  const tweet = async () => {
+    await statusesUpdate(token);
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,58 +40,17 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {isAuth ? (
+          <div>
+            <button onClick={() => logout()}>logout</button>
+            <button onClick={() => tweet()}>tweet</button>
+          </div>
+        ) : (
+          <div>
+            <button onClick={() => login()}>login</button>
+          </div>
+        )}
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
