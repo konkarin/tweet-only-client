63a2dab554ec9c5140f3c246c10f6cce4e2c6a44
konkarin <konkarin.photo@gmail.com>
2021-12-29 23:51:50 +0900

    update: TweetFormからIconListを切り出し

--- components/TweetForm.tsx
+++ components/TweetForm.tsx
@@ -3,6 +3,8 @@ import axios from "axios";
 import { useState } from "react";
 import styles from "../styles/TweetForm.module.scss";
 import IconList from "./IconList";
+import Button from "./Button/Button";
+import classNames from "classnames";
 
 interface Props {
   user: User;
@@ -11,7 +13,9 @@ interface Props {
 export default function TweetForm({ user }: Props) {
   const [inputValue, setInputValue] = useState("");
 
-  const post = async () => {
+  const post = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
     if (!user || inputValue === "") return;
 
     const url = "/api/post";
@@ -30,18 +34,13 @@ export default function TweetForm({ user }: Props) {
     setInputValue(e.target.value);
   };
 
-  const urls = [
-    "https://pbs.twimg.com/profile_images/1319312049957007363/GAY0jYhG_normal.jpg",
-    "https://pbs.twimg.com/profile_images/1147809083669413888/1vo1ib0O_normal.png",
-  ];
+  const counterClass = classNames(styles.tweetForm__count, {
+    [styles["tweetForm__count--over"]]: inputValue.length > 140,
+  });
 
   return (
-    <form onSubmit={() => post()}>
+    <form onSubmit={post}>
       <div className={styles.tweetForm}>
-        {/* user icons */}
-        <div className={styles.tweetForm__iconList}>
-          <IconList urls={urls} />
-        </div>
         {/* textarea */}
         <div className={styles.tweetForm__formWrapper}>
           <textarea
@@ -50,14 +49,13 @@ export default function TweetForm({ user }: Props) {
             className={styles.tweetForm__textarea}
             placeholder="What's the point of tweeting?"
           />
+          <div className={counterClass}>{140 - inputValue.length}</div>
         </div>
         {/* control */}
         <div className={styles.tweetForm__controlWrapper}>
           <div className={styles.tweetForm__control}></div>
           <div>
-            <button className={styles.tweetForm__tweet} type="submit">
-              Tweet
-            </button>
+            <Button type="submit">Tweet</Button>
           </div>
         </div>
       </div>
